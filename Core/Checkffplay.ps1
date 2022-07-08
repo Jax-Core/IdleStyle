198ae97fb95f91f99d62b8088be3f105a52291a1
Jax <enhancedjax@gmail.com>
2022-07-08 19:53:19 +0800

    v1.62

--- Core/Checkffplay.ps1
+++ Core/Checkffplay.ps1
@@ -6,8 +6,8 @@ function Update {
 
 function Check-ffplay {
     If (Test-Path -Path "$SkinsPath..\CoreData\IdleStyle\ffplay.exe") {
-        $RmAPI.Bang('[!SetOption Item2.StringIcon Text "[\xe73e]"][!SetOption Item2.String Text "Requirements met!"][!UpdateMeterGroup Ava][!Redraw]')
+        $RmAPI.Bang('[!SetOption Item2.StringIcon Text "[\xe73e]"][!SetOption Item2.String Text "Requirements met!"][!SetOptionGroup Ava FontColor "255,0,0"][!SetOptionGroup Ava FontWeight "600"][!SetOptionGroup Ava FontSize "(14*[Set.S])"][!UpdateMeterGroup Ava][!Redraw]')
     } else {
-        $RmAPI.Bang('[!SetOption Item2.StringIcon Text "[\xe711]"][!SetOption Item2.String Text "Requirements not met!"][!UpdateMeterGroup Ava][!Redraw]')
+        $RmAPI.Bang('[!SetOption Item2.StringIcon Text "[\xe711]"][!SetOption Item2.String Text "Requirements not met!"][!SetOptionGroup Ava FontColor "255,0,0"][!SetOptionGroup Ava FontWeight "600"][!SetOptionGroup Ava FontSize "(14*[Set.S])"][!UpdateMeterGroup Ava][!Redraw]')
     }
 }
